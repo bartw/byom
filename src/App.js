8df3d795282dd222eb775b0dea3505372421c608
Bart Wijnants <bart.w@sparkcentral.com>
2019-08-17 09:51:42 +0200

    more styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
 import Authentication from "./Authentication";
 import "./App.css";
 
 export default () => (
   <div className="app">
-    <h1>byom</h1>
-    <Authentication />
+    <header>
+      <h1>byom</h1>
+      <Authentication />
+    </header>
+    <main>
+      <div className="hero">
+        <h1>byom</h1>
+        <h2>bring your own music</h2>
+      </div>
+    </main>
+    <footer>
+      <div className="copyright">&copy; 2019 byom</div>
+      <a
+        href="https://github.com/bartw/byom"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="fork me"
+      >
+        <FontAwesomeIcon icon={faUtensils} fixedWidth />
+      </a>
+    </footer>
   </div>
 );
